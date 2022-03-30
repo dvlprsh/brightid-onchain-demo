73065d2c3f1160275eca9159a50a1e18aa482b74
Insun <inseonyu7@gmail.com>
2022-03-30 18:27:52 +0900

    chore: change page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>BrightID onchain group | Interep</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
