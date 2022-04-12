a0a6e7565e2cbe7d75a8aa7b0a8bdbf61abf52b1
dvlprsh <dvlprsh103@gmail.com>
2022-04-12 11:35:35 +0900

    fix: useBrightId hook

--- src/hooks/useBrightId.ts
+++ src/hooks/useBrightId.ts
@@ -1,16 +1,11 @@
 import { useCallback, useState } from "react"
-import { Signer, Contract, providers, Wallet, utils } from "ethers"
-import { OnchainAPI } from "@interep/api"
-import createIdentity from "@interep/identity"
+import { Contract, providers, Wallet } from "ethers"
 import Interep from "contract-artifacts/Interep.json"
 import BrightidInterep from "contract-artifacts/BrightidInterep.json"
 import getNextConfig from "next/config"
 import stringify from "fast-json-stable-stringify"
-import B64 from "base64-js"
-import tweetNacl from "tweetnacl"
-import tweetNaclUtils from "tweetnacl-util"
 import { sponsor } from "brightid_sdk"
-import { getAddress } from "ethers/lib/utils"
+import { VerificationsApiResponse } from "src/types"
 
 const provider = new providers.JsonRpcProvider(
   `https://kovan.infura.io/v3/${
@@ -41,8 +36,8 @@ type ReturnParameters = {
   getBrightIdUserData: (address: string) => Promise<any>
   selfSponsor: (address: string) => Promise<any>
   registerBrightId: (address: string) => Promise<any>
-  transactionHash: string
-  hasjoined: boolean
+  etherscanLink: string
+  transactionstatus: boolean
   loading: boolean
 }
 
@@ -59,14 +54,14 @@ const getMessage = (op: any) => {
 
 export default function useBrightId(): ReturnParameters {
   const [_loading, setLoading] = useState<boolean>(false)
-  const [_transactionHash, setTransactionHash] = useState<string>("")
-  const [_hasjoined, setHasjoined] = useState<boolean>(false)
+  const [_link, setEtherscanLink] = useState<string>("")
+  const [_transactionStatus, setTransactionStatus] = useState<boolean>(false)
 
   const getBrightIdUserData = useCallback(
-    async (address: string): Promise<string | null> => {
+    async (address: string): Promise<VerificationsApiResponse | null> => {
       try {
         const response = await fetch(
-          `https://app.brightid.org/node/v5/verifications/${CONTEXT}/${address}`
+          `https://app.brightid.org/node/v5/verifications/${CONTEXT}/${address}?signed=eth&timestamp=seconds`
         )
         return response.json()
       } catch (error) {
@@ -78,59 +73,41 @@ export default function useBrightId(): ReturnParameters {
     []
   )
 
-  //Todo: find component of signiture _v, _r, _s
   const registerBrightId = useCallback(async (address: string) => {
-    const transaction = await BrightidInterepContract.connect(
-      adminWallet
-    ).register(CONTEXT, address, Date.now(), "_v", "_r", "_s")
+    try {
+      setLoading(true)
+
+      const response = await getBrightIdUserData(address)
+
+      if (!response?.data) throw new Error()
+
+      const {
+        contextIds,
+        sig: { r, s, v },
+        timestamp
+      } = response?.data
+
+      const transaction = await BrightidInterepContract.connect(
+        adminWallet
+      ).register(CONTEXT, contextIds, timestamp, v, r, s)
+
+      const receipt = await provider.waitForTransaction(transaction.hash)
+
+      setTransactionStatus(!!receipt.status)
+      setEtherscanLink("https://kovan.etherscan.io/tx/" + transaction.hash)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      throw error
+    }
   }, [])
 
   const selfSponsor = useCallback(async (address: string) => {
     try {
-      const op = {
-        name: "Sponsor",
-        app: CONTEXT,
-        contextId: address,
-        timestamp: Date.now(),
-        v: 5,
-        sig: ""
-      }
-
-      // const message = getMessage(op)
-      // const messageUint8Array = Buffer.from(message)
-      // console.log("test", message, messageUint8Array)
-      // const privateKeyUint8Array = tweetNaclUtils.decodeBase64(privateKey)
-
-      // const signedMessageUint8Array = tweetNacl.sign.detached(
-      //   messageUint8Array,
-      //   privateKeyUint8Array
-      // )
-
-      // op.sig = tweetNaclUtils.encodeBase64(signedMessageUint8Array)
-
-      const message = getMessage(op)
-      const arrayedMessage = Buffer.from(message)
-      const keyBuffer = B64.toByteArray(privateKey)
-      let signature = tweetNacl.sign.detached(arrayedMessage, keyBuffer)
-      op.sig = B64.fromByteArray(signature)
-
-      // const response = await fetch(
-      //   "https://app.brightid.org/node/v5/operations",
-      //   {
-      //     method: "POST",
-      //     headers: {
-      //       Accept: "application/json",
-      //       "Content-Type": "application/json"
-      //     },
-      //     body: JSON.stringify(op)
-      //   }
-      // )
-
       const response = await sponsor(privateKey, CONTEXT, address)
-      console.log("sponsor response", response)
+
       return response
     } catch (error) {
-      console.error(error)
       setLoading(false)
       return null
     }
@@ -139,8 +116,8 @@ export default function useBrightId(): ReturnParameters {
     getBrightIdUserData,
     selfSponsor,
     registerBrightId,
-    transactionHash: _transactionHash,
-    hasjoined: _hasjoined,
+    etherscanLink: _link,
+    transactionstatus: _transactionStatus,
     loading: _loading
   }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,17 @@
 export type StrBigInt = string | bigint
+
+export interface VerificationsApiResponse {
+  data: {
+    app: string
+    context: string
+    contextIds: Array<string>
+    unique: boolean
+    sig: {
+      r: string
+      s: string
+      v: number
+    }
+    timestamp: number
+    publicKey: string
+  }
+}
