74904cb69a4bc09ad387a02811ff2254ba3d8225
dvlprsh <dvlprsh103@gmail.com>
2022-04-13 17:47:04 +0900

    feat: add wallet connect button to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,45 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import Image from "next/image"
 import AppBar from "@mui/material/AppBar"
 import Container from "@mui/material/Container"
 import Toolbar from "@mui/material/Toolbar"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
-import { useRouter } from "next/router"
-import Image from "next/image"
 import logo from "../img/logo.png"
+import shortenAddress from "src/utils/shortenAddress"
+
+import { useWeb3React } from "@web3-react/core"
+import { InjectedConnector } from "@web3-react/injected-connector"
+import { providers } from "ethers"
+
+const pages = [
+  { title: "Join/Leave Group", path: "/" },
+  { title: "Memebership Proof", path: "/proof" },
+  { title: "Mint NFT", path: "/mint" }
+]
+
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42]
+})
 
 export default function NavBar(): JSX.Element {
-  const pages = [
-    { title: "Join/Leave Group", path: "/" },
-    { title: "Memebership Proof", path: "/proof" },
-    { title: "Mint NFT", path: "/mint" }
-  ]
   const router = useRouter()
+  const { activate, account } = useWeb3React<providers.Web3Provider>()
+
+  useEffect(() => {
+    ;(async () => {
+      if (await injectedConnector.isAuthorized()) {
+        await activate(injectedConnector)
+      }
+    })()
+  }, [activate])
 
   return (
     <AppBar position="static" color="primary">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ width: 170, marginRight: 5}}>
+          <Box sx={{ width: 170, marginRight: 5 }}>
             <Image src={logo} alt="interep X BrihtID" layout="responsive" />
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
@@ -33,6 +53,19 @@ export default function NavBar(): JSX.Element {
               </Button>
             ))}
           </Box>
+          {account ? (
+            <Button variant="outlined" color="secondary">
+              {shortenAddress(account)}
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => activate(injectedConnector)}
+            >
+              Connect Wallet
+            </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
