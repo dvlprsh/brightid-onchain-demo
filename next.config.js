7fb00e949644b2d5fbbd17c6cd9d191995b4c6c5
dvlprsh <dvlprsh103@gmail.com>
2022-04-14 16:59:30 +0900

    test

--- next.config.js
+++ next.config.js
@@ -6,9 +6,22 @@ const nextConfig = {
     adminprivatekey: process.env.BRIGHTID_GROUP_ADMIN_PRIVATE_KEY,
     infuraApiKey: process.env.INFURA_API_KEY,
     brightIdApiKey: process.env.BRIGHTID_API_PRIVATE_KEY,
+    root: __dirname
   },
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.(zkey|wasm)$/i,
+      loader: "file-loader"
+    })
+
+    return config
+  },
+  typescript: {
+    ignoreBuildErrors: true
   }
 }
 
